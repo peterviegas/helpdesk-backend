e5d1a1a2374c293d68adad4214c7d4098ad9b0b4
Peter Viegas <peterviegas@outlook.com>
2022-07-10 19:23:51 +0100

    anotation JPA for classes of dommain

---
+++ src/main/java/com/peter/helpdesk/domain/Cliente.java
@@ -0,0 +1,34 @@
+package com.peter.helpdesk.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Cliente extends Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
+}
---
+++ src/main/java/com/peter/helpdesk/domain/Tecnico.java
@@ -0,0 +1,35 @@
+package com.peter.helpdesk.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class Tecnico extends Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Tecnico() {
+		super();
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
+}
