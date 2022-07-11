d4360646cc1fd55ff6a15d498f63a97290ab246a
Peter Viegas <peterviegas@outlook.com>
2022-07-12 00:54:23 +0100

    Excecao para objeto não encontrado

--- src/main/java/com/peter/helpdesk/services/TecnicoService.java
+++ src/main/java/com/peter/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.peter.helpdesk.domain.Tecnico;
 import com.peter.helpdesk.repositories.TecnicoRepository;
+import com.peter.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " +id));
 	}
 }
