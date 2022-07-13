29bc322e9aa256a99a0e0c5b3626144bd7f4d413
Peter Viegas <peterviegas@outlook.com>
2022-07-13 21:14:05 +0100

    Metodo put para o Técnico implementado

--- src/main/java/com/peter/helpdesk/services/TecnicoService.java
+++ src/main/java/com/peter/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.peter.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,6 +40,15 @@ public class TecnicoService {
 		return repository.save(newObj);
 	}
 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
+	
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if(obj.isPresent()&& obj.get().getId() != objDTO.getId()) {
@@ -49,4 +60,6 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}	
 	}
+
+
 }
