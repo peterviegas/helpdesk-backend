7d0066c984075573ef082630cdd23de03d988b93
Peter Viegas <peterviegas@outlook.com>
2022-07-13 09:17:38 +0100

    Endpoint para Tecnico criado

--- src/main/java/com/peter/helpdesk/domain/Tecnico.java
+++ src/main/java/com/peter/helpdesk/domain/Tecnico.java
@@ -3,11 +3,13 @@ package com.peter.helpdesk.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.peter.helpdesk.domain.dtos.TecnicoDTO;
 
 
 @Entity
@@ -24,6 +26,17 @@ public class Tecnico extends Pessoa implements Serializable {
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfil = obj.getPerfil().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/peter/helpdesk/services/TecnicoService.java
+++ src/main/java/com/peter/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.peter.helpdesk.domain.Tecnico;
+import com.peter.helpdesk.domain.dtos.TecnicoDTO;
 import com.peter.helpdesk.repositories.TecnicoRepository;
 import com.peter.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -23,5 +24,11 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null); //Para evitar bagunça no banco de dados
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
