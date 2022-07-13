c2b3832ea4ef53a62eb39fdeca4d5d801e786a99
Peter Viegas <peterviegas@outlook.com>
2022-07-13 18:28:05 +0100

    validaçao dos campos para criação do usuário criada

--- src/main/java/com/peter/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/peter/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.peter.helpdesk.domain.Tecnico;
 import com.peter.helpdesk.domain.enums.Perfil;
@@ -14,10 +16,13 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	protected Integer id;
-	
+	@NotNull(message = "O campo Nome é requerido")
 	protected String nome;	
+	@NotNull(message = "O campo CPF é requerido")
 	protected String cpf;
+	@NotNull(message = "O campo Email é requerido")
 	protected String email;
+	@NotNull(message = "O campo Senha é requerido")
 	protected String senha;
 	protected Set<Integer>perfil = new HashSet<>();
 	
